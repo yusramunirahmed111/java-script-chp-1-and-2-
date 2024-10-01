09e48f9f5c952d29941d91fb58e8489448a40a10
yusramunirahmed111 <sairayusra.2811@gmail.com>
2024-10-01 15:26:07 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+//CHAPTER NO 1 
+// Q1 What does the alert function do in JavaScript?
+// alert is a pop up messege 
+//Q2 Write the code to display an alert with the message "Welcome to my website!".
+alert("Welcome to my website!");
+// Q3 What happens if you capitalize the alert keyword?
+//If you capitalize the alert keyword in JavaScript (e.g., writing Alert), 
+//it will result in a reference error because JavaScript is case-sensitive.
+//Q4 Write the correct JavaScript syntax to display "Hello, world!" in an alert box.
+alert("Hello, world!");
+//Q5 Write a JavaScript alert statement with proper formatting and explain each part of the code
+alert("Welcome to my website!");
+//alert
+//This is the function name. It is a built-in JavaScript function used to display a modal dialog box with a specified message. The dialog will have an "OK" button.
+//( and ):
+//These are parentheses used to enclose the arguments passed to the function. In this case, we're passing a single argument: a string.
+//"Welcome to my website!":
+//This is the argument being passed to the alert function. It is a string that contains the message that will be displayed in the alert dialog. Strings in JavaScript can be enclosed in either double quotes (" "), single quotes (' '), or backticks (` ` for template literals).
+
+//CHAPTER NO 2 
+//Q1 Write code that declares a variable message, assigns the value "Hello, world!" to
+//it, and then displays an alert with that message
+var message = "Hello, world!";
+alert(message);
+//Q2 Declare a variable named age, leave it undefined, then assign the value 25 to it in a
+//later statement. Display an alert showing the value of age.
+var age = 
+age = 25
+alert(age);
+//Q3  Declare a variable named greeting, assign it the value "Good morning!", and then
+//display it in an alert. Afterwards, change the value of greeting to "Good evening!"
+//and display the new value in another alert.
+var greeting = "Good morning!";
+alert(greeting);
+greeting = "Good evening!";
+alert(greeting);
+//Q4 Create a variable called favoriteColor and assign it the value "blue". Then display
+//an alert that says "My favorite color is blue" using the variable.
+var favoriteColor ="blue" ;
+alert("My favorite color is " + favoriteColor);
